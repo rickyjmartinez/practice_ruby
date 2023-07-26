bc32222d0a1816267f547d1b1731974a81f67538
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-25 22:49:53 -0400

    add question 2 of practice

--- conditionals2_practice.rb
+++ conditionals2_practice.rb
@@ -1,8 +1,20 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-x = 11
+# x = 11
 
-if x == 10
-  p 0
-else
+# if x == 10
+#   p 0
+# else
+#   p -1
+# end
+
+# 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+x = 10
+
+if x < 10
   p -1
+elsif x > 10
+  p 1
+elsif x == 10
+  p 0
 end
