dbb4f381f36a4f912d4ce6bb91c31b5ab331448d
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-28 10:07:42 -0400

    add second problem of strings practice

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -1,6 +1,13 @@
 # 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
 
+# first_name = "ricky"
+# last_name = "martinez"
+
+# p first_name + " " + last_name
+
+# 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
+
 first_name = "ricky"
 last_name = "martinez"
 
-p first_name + " " + last_name
+p "#{first_name} #{last_name}"
