6e0214281b18b6e289ddfd027f40883e097744f8
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-28 10:22:03 -0400

    add fourth  problem of strings practice

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -14,8 +14,15 @@
 
 # 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
 
-puts "please enter a word"
-word = gets.chomp
-if word == "marco"
-  p "polo"
-end
+# puts "please enter a word"
+# word = gets.chomp
+# if word == "marco"
+#   p "polo"
+# end
+
+# 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+color1 = "red"
+color2 = "blue"
+color3 = "green"
+
+p "the three colors are " + color1 + ", " + color2 + ", " + "and " + color3
