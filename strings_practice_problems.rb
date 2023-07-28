8287b972d794c9b8440b45011b01495fa1740d42
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-28 10:11:19 -0400

    add third  problem of strings practice

--- strings_practice_problems.rb
+++ strings_practice_problems.rb
@@ -7,7 +7,15 @@
 
 # 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the #{} operator).
 
-first_name = "ricky"
-last_name = "martinez"
+# first_name = "ricky"
+# last_name = "martinez"
 
-p "#{first_name} #{last_name}"
+# p "#{first_name} #{last_name}"
+
+# 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+puts "please enter a word"
+word = gets.chomp
+if word == "marco"
+  p "polo"
+end
