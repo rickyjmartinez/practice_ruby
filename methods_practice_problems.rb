e309772b0cb2e1779c075a98f09b50571ae0d614
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 10:59:45 -0400

    add 9th problem of methods practice

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -43,8 +43,15 @@
 # p word
 
 # 8. Write a program that asks the user to enter a number, then prints "That's a negative number" if the number is less than 0.
-puts "please enter a number"
-number = gets.chomp.to_i
-if number < 0
-  puts "that's a negative number"
-end
+# puts "please enter a number"
+# number = gets.chomp.to_i
+# if number < 0
+#   puts "that's a negative number"
+# end
+
+# 9. Write a program that asks the user to enter two numbers, then prints the two numbers multiplied together.
+
+puts "please enter two numbers"
+number1 = gets.chomp.to_i
+number2 = gets.chomp.to_i
+p number1 * number2
