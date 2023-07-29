a6dc94811efcf200f6ee4ed16bb0b9cbc4aecc2c
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 10:11:29 -0400

    add 2nd problem of methods practice

--- methods_practice_problems.rb
+++ methods_practice_problems.rb
@@ -1,5 +1,13 @@
 # 1. Write a program that asks the user to enter a word, then prints that word with all capital letters.
 
-puts "please enter a word"
-word = gets.chomp.upcase
-p word
+# puts "please enter a word"
+# word = gets.chomp.upcase
+# p word
+
+# 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
+
+# puts "please enter a number"
+# number = gets.chomp.to_i
+# if number > 100
+#   p "that's a big number"
+# end
