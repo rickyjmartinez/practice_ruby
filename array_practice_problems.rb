a334aca8e06841c96486f47182cbe0d241219e44
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 23:53:18 -0400

    add 3rd problem of array practice

--- array_practice_problems.rb
+++ array_practice_problems.rb
@@ -5,6 +5,14 @@
 # p array
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
-array = ["a", "b", "c", "d"]
-array[1] = 1
-p array
+# array = ["a", "b", "c", "d"]
+# array[1] = 1
+# p array
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+# array = [0, 1, 2, 3, 4]
+# i = 0
+# while i < array.length
+#   p array[i]
+#   i += 1
+# end
