2d048a3bb56acd0850bd3d84990e051ba61405ae
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 21:09:41 -0400

    add 4th problem of loops practice

--- loops_practice.rb
+++ loops_practice.rb
@@ -1,22 +1,31 @@
 # 1. Write a while loop to print the numbers 1 through 10.
-i = 1
-while i <= 10
-  p i
-  i += 1
-end
+# i = 1
+# while i <= 10
+#   p i
+#   i += 1
+# end
 
 # 2. Write a while loop that prints the word "hello" 5 times.
-i = 0
-while i <= 5
-  p "hello"
-  i += 1
-end
+# i = 0
+# while i <= 5
+#   p "hello"
+#   i += 1
+# end
 
 # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
-while true
-  puts "enter a word"
-  word = gets.chomp.downcase
-  if word == "stop"
-    break
-  end
-end
+# while true
+#   puts "enter a word"
+#   word = gets.chomp.downcase
+#   if word == "stop"
+#     break
+#   end
+# end
+
+# 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+# i = 5
+
+# while i <= 100
+#   p i
+#   i += 5
+# end
