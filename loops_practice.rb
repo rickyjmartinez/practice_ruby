c0042e2e2814c12a3b6a1d814a39214dc11d8ff9
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 20:05:26 -0400

    add 2nd and 3rd problem of loops practice

--- loops_practice.rb
+++ loops_practice.rb
@@ -4,3 +4,19 @@ while i <= 10
   p i
   i += 1
 end
+
+# 2. Write a while loop that prints the word "hello" 5 times.
+i = 0
+while i <= 5
+  p "hello"
+  i += 1
+end
+
+# 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+while true
+  puts "enter a word"
+  word = gets.chomp.downcase
+  if word == "stop"
+    break
+  end
+end
