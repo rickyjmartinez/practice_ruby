df58cfd8cde72471b2d9045d109623e235f486f6
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 23:49:01 -0400

    add 2nd problem of array practice

--- array_practice_problems.rb
+++ array_practice_problems.rb
@@ -1,5 +1,10 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
-array = ["one", "two", "three"]
-array << "four"
-array << "five"
+# array = ["one", "two", "three"]
+# array << "four"
+# array << "five"
+# p array
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+array = ["a", "b", "c", "d"]
+array[1] = 1
 p array
