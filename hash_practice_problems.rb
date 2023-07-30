a48e72648ca863079887f1d17fee451d1383c75e
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 10:30:23 -0400

    add 4th problem of hash practice. update 2nd question

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -5,15 +5,22 @@
 # p person[:email]
 
 # 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
-array = [
-  { first: "ricky", last: "martinez" },
-  { first: "rick", last: "sanchez" },
-  { first: "morty", last: "smith" },
-]
-p array[0][:first]
-p array[0][:last]
+# array = [
+#   { first: "ricky", last: "martinez" },
+#   { first: "rick", last: "sanchez" },
+#   { first: "morty", last: "smith" },
+# ]
+# p array[0][:first]
+# p array[0][:last]
 
 # 3. Make a hash to store prices for 3 different menu items. Then add a new menu item and price and print the hash to see the result.
-menu = { hotdog: 1, burger: 2, fries: 1 }
-menu[:chips] = 1
-p menu
+# menu = { hotdog: 1, burger: 2, fries: 1 }
+# menu[:chips] = 1
+# p menu
+
+# 4. Make a hash to store a book's title, author, number of pages, and language. Then print each attribute on separate lines.
+book = { title: "the hobbit", author: "jrr tolkien", pages: 500, language: "english" }
+p book[:title]
+p book[:author]
+p book[:pages]
+p book[:language]
