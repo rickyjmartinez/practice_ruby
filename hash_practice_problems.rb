e90ac2c4adc8effdb722d03f4906f84c7dc59aa8
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 09:54:38 -0400

    add 1st problem of hash practice

---
+++ hash_practice_problems.rb
@@ -0,0 +1,5 @@
+# 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
+person = { first: "ricky", last: "martinez", email: "ricky@email.com" }
+p person[:first]
+p person[:last]
+p person[:email]
