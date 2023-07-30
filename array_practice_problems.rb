d2f6dc680a3f9553329fe98ab57b113fc47346b3
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-29 21:41:22 -0400

    add 1st problem of array practice

---
+++ array_practice_problems.rb
@@ -0,0 +1,5 @@
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+array = ["one", "two", "three"]
+array << "four"
+array << "five"
+p array
