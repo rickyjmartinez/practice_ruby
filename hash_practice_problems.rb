4e92b43bbe160a34848694009f8721d978f8dfd8
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 10:09:00 -0400

    add 2nd problem of hash practice

--- hash_practice_problems.rb
+++ hash_practice_problems.rb
@@ -1,5 +1,13 @@
 # 1. Make a hash to store a person's first name, last name, and email address. Then print each attribute on separate lines.
-person = { first: "ricky", last: "martinez", email: "ricky@email.com" }
-p person[:first]
-p person[:last]
-p person[:email]
+# person = { first: "ricky", last: "martinez", email: "ricky@email.com" }
+# p person[:first]
+# p person[:last]
+# p person[:email]
+
+# 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+array = [
+  { first: "ricky", last: "martinez" },
+  { first: "rick", last: "sanchez" },
+  { first: "morty", last: "smith" },
+]
+p array[0]
