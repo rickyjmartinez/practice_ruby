ed6b3575959b10f35ede9b015285e28f83e38ef4
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 20:38:00 -0400

    add 2nd and 3rd  problems of functions practice

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -4,3 +4,17 @@
 # end
 
 # p double(5)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+def capital(string)
+  string.upcase
+end
+
+p capital("string")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+def subtract(x, y)
+  x - y
+end
+
+p subtract(3, 4)
