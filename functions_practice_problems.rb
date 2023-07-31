2369bff011fa0ea9ad5d3bde5679831685ee5524
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 22:03:49 -0400

    add 10th problem of functions practice

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -55,8 +55,15 @@
 # p five_times("test")
 
 # 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
-def avg(x, y, z)
-  ((x + y + z) / 3.0).to_f
+# def avg(x, y, z)
+#   ((x + y + z) / 3.0).to_f
+# end
+
+# p avg(2, 3, 5)
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+def ten_plus_30(x)
+  (x * 10) + 30
 end
 
-p avg(2, 3, 5)
+p ten_plus_30(10)
