5f1debfe1b0d0cb81ed1e1a6519ef190f7556514
ricky martinez <rickyjmartinez2@gmail.com>
2023-07-30 20:40:15 -0400

    add 4th problem of functions practice

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -6,15 +6,22 @@
 # p double(5)
 
 # 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
-def capital(string)
-  string.upcase
-end
+# def capital(string)
+#   string.upcase
+# end
 
-p capital("string")
+# p capital("string")
 
 # 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
-def subtract(x, y)
-  x - y
+# def subtract(x, y)
+#   x - y
+# end
+
+# p subtract(3, 4)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+def square(x)
+  x * x
 end
 
-p subtract(3, 4)
+p square(4)
