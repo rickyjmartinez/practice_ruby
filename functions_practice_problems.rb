05b07f32b2bfd356220f47e349cf90c1c695fe54
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-07 15:55:58 -0400

    ruby function practice

--- functions_practice_problems.rb
+++ functions_practice_problems.rb
@@ -67,3 +67,83 @@ def ten_plus_thirty(x)
 end
 
 p ten_plus_thirty(10)
+
+# 1. Write a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+def timestwo(x)
+  x * 2
+end
+
+p timestwo(4)
+
+# 2. Write a function that takes in a string and returns the string with all capital letters. Then run the function and print the result.
+
+def capitalize(string)
+  string.upcase
+end
+
+p capitalize("cat")
+
+# 3. Write a function that takes in two numbers and returns the first number subtracted by the second. Then run the function and print the result.
+
+def substract(x, y)
+  x - y
+end
+
+p substract(3, 4)
+
+# 4. Write a function that takes in a number and returns the number times itself. Then run the function and print the result.
+
+def square(x)
+  x * x
+end
+
+p square(2)
+
+# 5. Write a function that takes in a string and returns the first letter of the string. Then run the function and print the result.
+
+def firstletter(string)
+  string[0]
+end
+
+p firstletter("cat")
+
+# 6. Write a function that takes in three strings and returns a string that combines all three strings with spaces in between. Then run the function and print the result.
+
+def threewords(x, y, z)
+  "#{x} #{y} #{z}"
+end
+
+p threewords("cat", "dog", "bird")
+
+# 7. Write a function that takes in a number and returns the number as a string. Then run the function and print the result.
+
+def numbertostring(x)
+  x.to_s
+end
+
+p numbertostring(4)
+
+# 8. Write a function that takes in a string and returns the string repeated 5 times. Then run the function and print the result.
+
+def repeatfive(x)
+  x + x + x + x + x
+end
+
+p repeatfive("cat")
+
+# 9. Write a function that takes in 3 numbers and returns the average (the sum divided by 3.0). Then run the function and print the result.
+
+def avg(x, y, z)
+  ((x + y + z) / 3.0).to_f
+end
+
+p avg(1, 2, 4)
+
+# 10. Write a function that takes in a number and returns the number times 10 plus 30. Then run the function and print the result.
+
+def timestenplusthirty(x)
+  x * 10 + 30
+end
+
+p timestenplusthirty(10)
