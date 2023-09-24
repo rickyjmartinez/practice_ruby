077d517f5854999c122fa0bc2bf02682d0689258
ricky martinez <rickyjmartinez2@gmail.com>
2023-09-24 17:32:53 -0400

    hash practice problems1-3

--- whiteboarding.rb
+++ whiteboarding.rb
@@ -1,18 +1,2 @@
-def compareTriplets(a, b)
-  alice = 0 
-  bob = 0 
-  i = 0 
-  
-  while i < a.length 
-    if a[i] > b[i] 
-      alice += 1 
-    else 
-      bob += 1 
-    end 
-    i++
-  end 
-  alice 
-  bob
-end
 
-p compareTriplets([5,6,7], [3,6,10])
+
