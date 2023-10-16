0e5ae48f23e1c919902649c20287b31d626cd381
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-16 14:57:05 -0400

    whiteboarding algorhitm problems

--- whiteboarding.rb
+++ whiteboarding.rb
@@ -1,2 +1,17 @@
+# 1) Write a function that takes in an array of numbers and returns its sum.
+
+#input [a,b,c] output sum = a + b + c 
 
 
+
+def total(x)  
+  sum = 0 
+  i = 0 
+  while i < x.length 
+    sum += x[i]
+    i += 1
+  end 
+  sum
+end 
+
+p total([1,2,3,4])
