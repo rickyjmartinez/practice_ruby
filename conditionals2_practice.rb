e54d5b0084a406c37817acb8e125b7f35da2272b
ricky martinez <rickyjmartinez2@gmail.com>
2023-10-20 01:06:19 -0400

    conditional practice problems

--- conditionals2_practice.rb
+++ conditionals2_practice.rb
@@ -1,112 +1,135 @@
-# # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
-# x = 11
+# # # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+# # x = 11
 
-# if x == 10
-#   p 0
-# else
-#   p -1
-# end
+# # if x == 10
+# #   p 0
+# # else
+# #   p -1
+# # end
+
+# # # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+# # x = 10
+
+# # if x < 10
+# #   p -1
+# # elsif x > 10
+# #   p 1
+# # else
+# #   p 0
+# # end
+
+# # # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+# # x = 5
+# # y = 7
+
+# # if x < 10 && y < 10
+# #   p 1
+# # else
+# #   p 0
+# # end
+
+# # # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+# # x = 9001
+
+# # if x > 9000
+# #   p "IT'S OVER 9000!!!" #havin a lil fun with it
+# # else
+# #   p -1
+# # end
+
+# # # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+# # x = 29
+
+# # if x < 10
+# #   p 9
+# # elsif x < 20
+# #   p 19
+# # elsif x < 30
+# #   p 29
+# # end
+
+# # # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+# # x = 5
+# # y = 5
+
+# # if x > 10 || y > 10
+# #   p 100
+# # else
+# #   p -100
+# # end
+
+# # # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+# # x = -1
+
+# # if x < 0
+# #   p 1776
+# # else
+# #   p 1979
+# # end
+
+# # # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+# # x = 98
+
+# # if x == 100
+# #   p 100
+# # elsif x == 99
+# #   p 99
+# # else
+# #   p 0
+# # end
+
+# # # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+# # x = -1
+# # y = 34
+
+# # if x < 0 && y > 0
+# #   p 1
+# # else
+# #   p 0
+# # end
+
+# # # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+# # x = 20
+
+# # if x > 80
+# #   p 5
+# # elsif x > 60
+# #   p 4
+# # elsif x > 40
+# #   p 3
+# # elsif x > 20
+# #   p 2
+# # else
+# #   p 1
+# # end
+
+
+# # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+# x = 10 
+
+# if x == 10 
+#   p 0 
+# else 
+#   p -1 
+# end 
 
 # # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
-# x = 10
 
-# if x < 10
-#   p -1
-# elsif x > 10
-#   p 1
-# else
-#   p 0
-# end
+# x = 17
 
-# # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
-# x = 5
-# y = 7
-
-# if x < 10 && y < 10
-#   p 1
-# else
-#   p 0
-# end
-
-# # 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
-# x = 9001
-
-# if x > 9000
-#   p "IT'S OVER 9000!!!" #havin a lil fun with it
-# else
-#   p -1
-# end
-
-# # 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
-# x = 29
-
-# if x < 10
-#   p 9
-# elsif x < 20
-#   p 19
-# elsif x < 30
-#   p 29
-# end
-
-# # 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
-# x = 5
-# y = 5
-
-# if x > 10 || y > 10
-#   p 100
-# else
-#   p -100
-# end
-
-# # 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
-# x = -1
-
-# if x < 0
-#   p 1776
-# else
-#   p 1979
-# end
-
-# # 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
-# x = 98
-
-# if x == 100
-#   p 100
-# elsif x == 99
-#   p 99
-# else
-#   p 0
-# end
-
-# # 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
-# x = -1
-# y = 34
-
-# if x < 0 && y > 0
-#   p 1
-# else
-#   p 0
-# end
-
-# # 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
-# x = 20
-
-# if x > 80
-#   p 5
-# elsif x > 60
-#   p 4
-# elsif x > 40
-#   p 3
-# elsif x > 20
-#   p 2
-# else
-#   p 1
-# end
+# if x < 10 
+#   p -1 
+# elsif x > 10 
+#   p 1 
+# else 
+#   p 0 
+# end 
 
 
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-x = 10 
+x = 10
 
 if x == 10 
   p 0 
@@ -116,12 +139,101 @@ end
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
 
-x = 17
-
+x = 11
 if x < 10 
   p -1 
 elsif x > 10 
   p 1 
 else 
+  p 0
+end 
+
+# 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+x = 1
+y = 10
+
+if x < 10 && y < 10 
+  p 1 
+else 
   p 0 
+end 
+
+# 4. Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+x = 9001 
+if x > 9000 
+  p "IT'S OVER 9000"
+else 
+  p -1 
+end 
+
+# 5. Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+x = 30
+if x < 10 
+  p 9 
+elsif x < 20
+  p 19
+elsif x < 30 
+  p 29 
+else 
+  p -1 
+end 
+
+# 6. Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+x = 11
+y = 5 
+
+if x > 10 || y > 10 
+  p 100 
+else 
+  p -100
+end 
+
+# 7. Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+x = -1 
+if x < 0
+  p 1776
+else 
+  p 1979 
+end 
+# 8. Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+x = 99 
+if x == 100 
+  p 100 
+elsif x == 99 
+  p 99 
+else 
+  p 0
+end 
+
+# 9. Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+x = -1 
+y = 1 
+
+if x < 0 && y > 0 
+  p 1 
+else 
+  p 0
+end 
+
+# 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
+
+x = 21
+
+if x > 80 
+  p 5 
+elsif x > 60 
+  p 4
+elsif x > 40 
+  p 3
+elsif x > 20 
+  p 2 
+else 
+  p 1 
 end 
--- whiteboarding.rb
+++ whiteboarding.rb
@@ -49,18 +49,49 @@
 
 # 4) Write a function that takes in an array of words and returns the number of words that begin with the letter “a”.
 
-def a_words(array)
-  i = 0 
-  a = []
-  while i < array.length
-    if array[i][0] == "a" 
-      a << array[i]
-    end
-    i += 1
-  end 
-  a
-end 
+# def a_words(array)
+#   i = 0 
+#   a = []
+#   while i < array.length
+#     if array[i][0] == "a" 
+#       a << array[i]
+#     end
+#     i += 1
+#   end 
+#   a
+# end 
 
-p a_words(["banana","apple","a","cat","ardvark"])
+# p a_words(["banana","apple","a","cat","ardvark"])
 
+
+# 5) Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+
+# def single_string(array)
+#   single = ""
+#   i = 0
+#   while i < array.length
+#     single += array[i]
+#     if i < array.length - 1 
+#       single += ","
+#     end 
+#     i += 1 
+#   end 
+#   single
+# end 
+
+# p single_string(["apple","banana","orange"])
+
+# 6) Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+
+# def product(array)
+#   product = 1
+#   array.each do |num| 
+#     product = product * num
+#   end 
+#   product 
+# end 
+
+# p product([1,5,10])
+
+# 7) Write a function that takes in an array of numbers and returns the two smallest numbers.
 
