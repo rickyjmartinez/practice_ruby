be72e2e0718c0782bbaeb13a4e367a5d4e9d6735
ricky martinez <rickyjmartinez2@gmail.com>
2023-11-28 17:31:35 -0500

    ruby array practice

--- array_practice_problems.rb
+++ array_practice_problems.rb
@@ -131,6 +131,79 @@
 # end
 # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
-country = ["USA", "Mexico", "Canada"]
-country << "France"
-p country
+# country = ["USA", "Mexico", "Canada"]
+# country << "France"
+# p country
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+words = ["cat","dog","fish"]
+words << "bird"
+words << "cow"
+p words 
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+array = ["a","b","c","d"]
+array[1] = 2
+p array
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+array = [0,1,2,3,4]
+i = 0 
+while i < array.length 
+  p array[i]
+  i += 1 
+end 
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+array = [0] 
+array << 1
+array << 2
+array << 3 
+p array 
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+words = ["cat", "dog", "fish"] 
+words[2] = words[2].upcase
+p words
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+names = ["bill", "bob", "ben"] 
+i = 0 
+while i < names.length 
+  p names[i]
+  i += 1 
+end 
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+words = ["box", "book"]
+words << "binder"
+p words
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+numbers = [1,2,3,4,5]
+numbers[0] = numbers[0] * 10
+p numbers
+
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+numbers = [1,2]
+i = 0 
+while i < numbers.length
+  p numbers[i]
+  i += 1
+end 
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["usa", "mexico", "canada"]
+countries << "brazil"
+p countries
